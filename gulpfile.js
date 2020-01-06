3bd7d5f79a79f77856352823c8f55cb4db85b106
nummyn0rih <nummyn0rih@gmail.com>
2020-01-06 23:41:22 +0300

    structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,79 @@
 const gulp = require('gulp');
-const debug = require('gulp-debug');
 const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const path = require('path');
+const url = require('gulp-css-url-adjuster');
+const autoprefixer = require('gulp-autoprefixer');
+const postcss = require('gulp-postcss');
+// const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+// const minifyCss = require('gulp-minify-css');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('build', () => {
-  return gulp.src('common.blocks/button/*.css')
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('dist'))
-    .pipe(debug());
+const params = {
+  out: 'public',
+  htmlSrc: 'buttons.html',
+  levels: ['common.blocks, desktop.blocks'],
+};
+
+gulp.task('default', ['server', 'build']);
+
+gulp.task('server', () => {
+  browserSync.init({
+    server: params.out
+  });
+
+  gulp.watch('*.html', ['html']);
+  gulp.watch(params.levels.map((level) => {
+    const cssGlob = level + '/**/*.css';
+    return cssGlob;
+  }), ['css']);
 });
 
-gulp.task('watch', () => {
-  gulp.watch('common.blocks/button/*.css', gulp.series('build'));
+gulp.task('build', [ 'css']);
+
+// gulp.task('html', () => {
+//   gulp.src(params.htmlSrc)
+//   .pipe(rename('index.html'))
+//   .pipe(gulp.dest(params.out))
+//   .pipe(reload({ stream: true }));
+// });
+
+gulp.task('css', () => {
+  gulp.src(['common.blocks/**/*.css', 'desktop.blocks/**/*.css'])
+  .pipe(sourcemaps.init())
+  .pipe(concat('styles.css'))
+  .pipe(url({ prepend: 'images/'}))
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(params.out))
+  .pipe(reload({ stream: true }));  
 });
+
+// gulp.task('images', () => {
+//   gulp.src(source.dirs.map((dir) => {
+//     const imgGlobe = path.resolve(dir) + '/*.{jpg,png,svg}';
+//     return imgGlobe;
+//   }))
+//   .pipe(imagemin({
+//     progressive: true,
+//     svgoPlugins: [{ removeViewBox: false }],
+//     use: [ pngquant() ],
+//     interlaced: true
+//   }))
+//   .pipe(gulp.dest(path.join(params.out, 'images')));
+// });
+
+// gulp.task('clean', function (cb) {
+//   rimraf(path.clean, cb);
+// });
+
+// gulp.task('build', () => {
+//   return gulp.src('common.blocks/button/*.css')
+//     .pipe(concat('main.css'))
+//     .pipe(gulp.dest('public'))
+//     .pipe(debug());
+// });
