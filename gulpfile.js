9497bbf26634cacb3de6a20dcff4294f8bd5ffe3
Ivan Malshchukov <nummyn0rih@gmail.com>
2020-01-07 00:32:01 +0300

    fix gulp.task

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,13 @@ const reload = browserSync.reload;
 const path = require('path');
 const url = require('gulp-css-url-adjuster');
 const autoprefixer = require('gulp-autoprefixer');
-const postcss = require('gulp-postcss');
+// const postcss = require('gulp-postcss');
 // const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 // const minifyCss = require('gulp-minify-css');
 const sourcemaps = require('gulp-sourcemaps');
+const debug = require('gulp-debug');
 
 const params = {
   out: 'public',
@@ -19,21 +20,33 @@ const params = {
   levels: ['common.blocks, desktop.blocks'],
 };
 
-gulp.task('default', ['server', 'build']);
-
-gulp.task('server', () => {
-  browserSync.init({
-    server: params.out
-  });
-
-  gulp.watch('*.html', ['html']);
-  gulp.watch(params.levels.map((level) => {
-    const cssGlob = level + '/**/*.css';
-    return cssGlob;
-  }), ['css']);
+gulp.task('default', () => {
+  return gulp.src('common.blocks/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('styles.css'))
+    // .pipe(url({ prepend: 'images/'}))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(params.out))
+    .pipe(debug());
+    // .pipe(reload({ stream: true }));  
 });
 
-gulp.task('build', [ 'css']);
+// gulp.task('default', ['server', 'build']);
+
+// gulp.task('server', () => {
+//   browserSync.init({
+//     server: params.out
+//   });
+
+//   gulp.watch('*.html', ['html']);
+//   gulp.watch(params.levels.map((level) => {
+//     const cssGlob = level + '/**/*.css';
+//     return cssGlob;
+//   }), ['css']);
+// });
+
+// gulp.task('build', [ 'css']);
 
 // gulp.task('html', () => {
 //   gulp.src(params.htmlSrc)
@@ -42,16 +55,16 @@ gulp.task('build', [ 'css']);
 //   .pipe(reload({ stream: true }));
 // });
 
-gulp.task('css', () => {
-  gulp.src(['common.blocks/**/*.css', 'desktop.blocks/**/*.css'])
-  .pipe(sourcemaps.init())
-  .pipe(concat('styles.css'))
-  .pipe(url({ prepend: 'images/'}))
-  .pipe(postcss([ autoprefixer() ]))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest(params.out))
-  .pipe(reload({ stream: true }));  
-});
+// gulp.task('css', () => {
+//   gulp.src(['common.blocks/**/*.css', 'desktop.blocks/**/*.css'])
+//   .pipe(sourcemaps.init())
+//   .pipe(concat('styles.css'))
+//   .pipe(url({ prepend: 'images/'}))
+//   .pipe(postcss([ autoprefixer() ]))
+//   .pipe(sourcemaps.write())
+//   .pipe(gulp.dest(params.out))
+//   .pipe(reload({ stream: true }));  
+// });
 
 // gulp.task('images', () => {
 //   gulp.src(source.dirs.map((dir) => {
@@ -71,9 +84,9 @@ gulp.task('css', () => {
 //   rimraf(path.clean, cb);
 // });
 
-// gulp.task('build', () => {
-//   return gulp.src('common.blocks/button/*.css')
-//     .pipe(concat('main.css'))
-//     .pipe(gulp.dest('public'))
-//     .pipe(debug());
-// });
+gulp.task('build', () => {
+  return gulp.src('common.blocks/button/*.css')
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('public'))
+    .pipe(debug());
+});
